60719bd4e7fee76b076e474619fbc72175d5b9cd
jsanc623 <juan.sanchez@juanleonardosanchez.com>
2015-10-16 16:13:22 -0400

    Add initial migrations

---
+++ app/database/migrations/2015_10_16_200913_create_tower.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTower extends Migration {
+    var $table = 'tower';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->increments('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop($this->table);
+    }
+}
---
+++ app/database/migrations/2015_10_16_200915_unit_package.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UnitPackage extends Migration {
+    var $table = 'unit_package';
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->increments('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop($this->table);
+    }
+
+}
